f4f52ff45082c40c2aedc57a84d67799aa70d7dd
NurMi App <sasha.rostovskaya@bk.ru>
2024-08-16 13:36:35 +0300

    Create Chat Java

---
+++ TEXT/Chat.java
@@ -0,0 +1,9 @@
+// Import this Bubble Message.
+<activity
+  android:name=".bubbles.BubbleActivity"
+  android:theme="@style/AppTheme.NoActionBar"
+  android:label="@string/title_activity_bubble"
+  android:allowEmbedded="true"
+  android:resizeableActivity="true"
+/>
+  
